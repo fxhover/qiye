8a1738bf0951bc849411b8f91d4b216299c8e930
fxhover <736698959@qq.com>
2016-10-19 20:45:36 +0800

    title change

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>  人才招聘 - 北京格瑞森科技有限公司</title>
+<title> 北京格瑞森科技有限公司</title>
 <meta name="keywords" content="各种铝型材及卡槽卡簧等、喷灌管材及设备、净水过滤装置、施肥装置、热风炉、遮阳网及各种农膜、轴流风机、湿帘、补光灯、温室专" />
 <meta name="description" content="公司自成立以来，已经走过了十余载的风风雨雨，公司逐步由小变大，由弱变强；但公司坚持诚实守信、精益求精、勇于创新的理念始终未改变。你的满意，是我公司最大的心愿和追求！" />
 <link href="http://www.chinagreengrow.com/wp-content/themes/0115glgl/images/css.css" rel="stylesheet" type="text/css">
